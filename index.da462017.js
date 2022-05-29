5f2646e8460bd8b9a1328dc2ce13d53bd2af4d12
evgeniyablyoskina <100150587+evgeniyablyoskina@users.noreply.github.com>
2022-05-29 19:52:26 +0000

    Deploying to gh-pages from @ evgeniyablyoskina/donut@051590c69d5ce35dbad2e5988ddd7170fd7558c0 🚀

---
+++ index.da462017.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"fraction"},grabCursor:!0,spaceBetween:32,initialSlide:1,centeredSlides:!0,loop:!0,speed:500,effect:"slides",breakpoints:{320:{slidesPerView:1},768:{slidesPerView:3}}});
+//# sourceMappingURL=index.da462017.js.map
