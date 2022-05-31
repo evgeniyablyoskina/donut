e0eb71051837f015fc89e38e3e602697f10fb11f
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-01 00:23:10 +0300

    дороблений функціонал мобільного меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,21 +1,32 @@
 (() => {
+  // Отримуємо три об'єкти - кнопку, меню і список посилань
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const menuLinks = document.querySelectorAll('.site-nav__link');
+
+  //Подія кліку по кнопці
   menuBtnRef.addEventListener('click', () => {
+
+    // Отримуємо атрибут aria-expanded
     const expended = menuBtnRef.getAttribute('aria-expended') === 'true' || false;
+    // Переключаємо наявність класу is-open на кнопці
     menuBtnRef.classList.toggle('is-open');
+    //міняємо значення aria-expanded
     menuBtnRef.setAttribute('aria-expended', !expended);
 
+    // Переключаємо наявність класу is-open на меню
     mobileMenuRef.classList.toggle('is-open');
   });
-})();
 
-const menuLinks = document.querySelectorAll('.site-nav__link');
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener('click', () => {
-    const mobileMenuRef = document.querySelector('[data-menu]');
-    if(mobileMenuRef.classList.contains('is-open')){
-      mobileMenuRef.classList.remove('is-open');
-    }
+  // Створюємо подію кліку для кожного лінка
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', () => {
+
+      // Якщо мобільне меню відкрите закриваємо його і повертаємо бургер кнопку
+      if(mobileMenuRef.classList.contains('is-open')){
+        mobileMenuRef.classList.remove('is-open');
+        menuBtnRef.classList.remove('is-open');
+      }
+    });
   });
-});
+})();
