8a3a8a07f1f68f31b85034a6841e2a79739077a4
evgeniyablyoskina <100150587+evgeniyablyoskina@users.noreply.github.com>
2022-05-31 21:27:35 +0000

    Deploying to gh-pages from @ evgeniyablyoskina/donut@125ee548917fa37c4006a2c963c7b58d96788256 🚀

--- index.b445dbc7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expended")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expended",!n),t.classList.toggle("is-open")}))}();var menuLinks=document.querySelectorAll(".site-nav__link");menuLinks.forEach((function(e){e.addEventListener("click",(function(){var e=document.querySelector("[data-menu]");e.classList.contains("is-open")&&e.classList.remove("is-open")}))}));
-//# sourceMappingURL=index.b445dbc7.js.map
